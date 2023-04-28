471822251138d09a65779927d12ab62c0fd88220
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-04-27 19:34:49 -0700

    Update PortMap.py
    Add color sensors and change drivebase wheel diameter

--- PortMap.py
+++ PortMap.py
@@ -1,10 +1,12 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Icon
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 hub = PrimeHub()
 MotorLeft = Motor(Port.E,Direction.COUNTERCLOCKWISE) 
 MotorRight = Motor(Port.A,Direction.CLOCKWISE)
-driveBase = DriveBase(MotorLeft,MotorRight,56,113)
+driveBase = DriveBase(MotorLeft,MotorRight,56*0.95,113) #56, 113
+colorSensorLeft=ColorSensor(Port.F)
+colorSensorRight=ColorSensor(Port.B)
