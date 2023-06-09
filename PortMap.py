afd9d7becf2a1d8fa3d0014d617f29dd1176223c
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-06-08 19:21:04 -0600

    Add files via upload

--- PortMap.py
+++ PortMap.py
@@ -3,27 +3,10 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
-#print('PortMap.py running')
+
 hub = PrimeHub()
-MotorLeft = Motor(Port.E,Direction.COUNTERCLOCKWISE) 
+MotorLeft = Motor(Port.E,Direction.COUNTERCLOCKWISE)
 MotorRight = Motor(Port.A,Direction.CLOCKWISE)
-driveBase = DriveBase(MotorLeft,MotorRight,56,115)
-colorSensorLeft= ColorSensor(Port.F)
-colorSensorRight= ColorSensor(Port.B)
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
+driveBase = DriveBase(MotorLeft,MotorRight,56,113)
+colorSensorLeft=ColorSensor(Port.F)
+colorSensorRight=ColorSensor(Port.B)
