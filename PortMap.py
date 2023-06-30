f8054224d6fb9c6d851eb5a5a8f9bef42266f310
thebuilder6 <43260133+thebuilder6@users.noreply.github.com>
2023-06-29 20:47:49 -0600

    Added Cross System Support for Portmap
    @UxuginPython This was an attempt to clean up your port map changes, but I did not really understand what you we going for in the Devices class
    
    Co-Authored-By: UxuginPython <84234447+UxuginPython@users.noreply.github.com>

--- PortMap.py
+++ PortMap.py
@@ -1,16 +1,61 @@
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-hub = PrimeHub()
-motorLeft = Motor(Port.E,Direction.COUNTERCLOCKWISE) 
-motorRight = Motor(Port.A,Direction.CLOCKWISE)
-driveBase = DriveBase(motorLeft,motorRight,56,115)
 try:
-    colorSensorLeft=ColorSensor(Port.F)
-    colorSensorRight=ColorSensor(Port.B)
-    name = "Main"
-except:
-    name = "Bug"
+    from pybricks.hubs import *
+    from pybricks.parameters import *
+    from pybricks.pupdevices import *
+    from pybricks.robotics import *
+    from pybricks.tools import *
+except Exception:
+    pass
+
+def hubType():
+    try:
+        hub = CityHub()
+        return 'city'
+    except: pass
+    try:
+        hub = MoveHub()
+        return 'move'
+    except: pass
+    try:
+        hub = EV3Brick()
+        return 'EV3'
+    except: pass
+    try:
+        hub = TechnicHub()
+        return 'technic'
+    except: pass 
+    try:
+        hub = PrimeHub()
+        return 'prime'
+    except: pass
+        try:
+        hub = EssentialHub()
+        return 'essential'
+    except: pass
+    return 'virtual'
+
+hubName = hubType()
+
+if hubName == 'city':
+    pass
+if hubName == 'move':
+    motorLeft=Motor(Port.B, Direction.COUNTERCLOCKWISE)
+    motorRight=Motor(Port.A, Direction.CLOCKWISE)
+    drivebase=DriveBase(motorLeft, motorRight, 50, 50)
+if hubName == 'EV3':
+    pass
+if hubName == 'technic':
+    pass
+if hubName == 'prime':
+    motorRight=Motor(Port.A, Direction.CLOCKWISE)
+    motorLeft=Motor(Port.E, Direction.COUNTERCLOCKWISE)
+    try:
+        drivebase=DriveBase(motorLeft, motorRight, 56, 114)
+        colorSensorLeft=ColorSensor(Port.F)
+        colorSensorLight=ColorSensor(Port.B)
+        hubName += 'snow'
+    except:
+        drivebase=DriveBase(motorLeft, motorRight, 50, 50)
+        hubName += 'bug'
+if hubName == 'essential':
+    pass
