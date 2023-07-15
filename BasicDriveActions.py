41f19a8700231e0d42a34d57143d35a48397048f
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-07-15 17:45:32 -0400

    Add files via upload

--- BasicDriveActions.py
+++ BasicDriveActions.py
@@ -43,28 +43,38 @@ class DriveTurnAction(Action):
 
 #make a Action that drives to a point like the functions in new.py using sub actions shown above hint look at the SeriesAction 
 class GoToPoint_StartTurn(DriveTurnAction):
-    def __init__(self, location, destination):
+    def __init__(self, selfself):
         # creating a vector between location and destination
-        vector = tuple((destination.x-location.x, destination.y-location.y))
+        vector = tuple((selfself.destination.x-selfself.location.x,
+                       selfself.destination.y-selfself.location.y))
         # using the arc tangent to detirmine the angle of the vector
         direction = jmath.atan2(vector[0], vector[1])
         # detirmine the shortest correction between our current angle and the angle of the shortest path
-        turn = jmath.shortestDirectionBetweenBearings(direction, location.a)
+        turn = jmath.shortestDirectionBetweenBearings(direction, selfself.location.a)
         self.angle = turn
         self.vector = vector
         self.direction = direction
+        selfself.angle = turn
+        selfself.vector = vector
+        selfself.direction = direction
 
 
 class GoToPoint_Straight(DriveStraightAction):
-    def __init__(self, vector):
-        self.distance = (vector[0]**2+vector[1]**2)**0.5  # drive along the vector with magnitude
+    def __init__(self, selfself):
+        # drive along the vector with magnitude
+        self.distance = (selfself.vector[0]**2+selfself.vector[1] ** 2)**0.5
 
 
 class GoToPoint_EndTurn(DriveTurnAction):
-    def __init__(self, destination, direction):
+    def __init__(self, selfself):
         # turning to final orientation should be inline with destination
-        self.angle = jmath.shortestDirectionBetweenBearings(destination.a, direction)
+        self.angle = jmath.shortestDirectionBetweenBearings(
+            selfself.destination.a, selfself.direction)
 
 
-GoToPoint = SeriesAction(GoToPoint_StartTurn, GoToPoint_Straight, GoToPoint_EndTurn)
-# first attempt - doesn't work yet
+GoToPoint = SeriesAction()
+GoToPoint = SeriesAction(GoToPoint_StartTurn(GoToPoint),
+                         GoToPoint_Straight(GoToPoint),
+                         GoToPoint_EndTurn(GoToPoint))
+for i in enumerate(GoToPoint.mRemainingActions):
+    GoToPoint.mRemainingActions[i[0]].selfself = GoToPoint
