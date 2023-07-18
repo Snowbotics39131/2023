ace91153fad9854bd62653f17ad3baf0abd26f5e
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-07-17 20:46:53 -0400

    Delete jmath.py
    This is used.

--- Not Currently Used/jmath.py
+++
@@ -1,8 +0,0 @@
-import umath
-
-def shortestDirectionBetweenBearings(target,current): # finds the shortest point from a to b between two points on a circle 
-    return ((target-current+540)%360-180)
-    #180 is always -180
-
-def atan2(b,a): # arc tangent version 2 converts from degrees to radians
-    return (umath.atan2(b,a)/umath.pi*180) #
