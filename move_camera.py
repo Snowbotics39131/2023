564eff511daa26907d93df3a0d3302b516966325
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-09-14 20:11:39 -0600

    Add files via upload

--- move_camera.py
+++ move_camera.py
@@ -50,7 +50,7 @@ class MoveCamera(MissionBase):
 #attachment up
 class Dragon(MissionBase):
     def routine(self):
-        self.runAction(DriveTurnAction(45))
+        self.runAction(DriveTurnAction(30))
 #near red home
 #580mm north
 #230mm east
@@ -62,9 +62,9 @@ class GetToPink(MissionBase):
         if self.color=='blue':
             times=0
         elif self.color in ('orange', 'yellow'):
-            times=2
-        elif self.color=='pink':
             times=1
+        elif self.color=='pink':
+            times=2
         else:
             raise ValueError(f'Invalid color: {self.color}')
         for i in range(times):
@@ -94,8 +94,8 @@ class Chicken(MissionBase):
                 DriveStraightAction(30)
             ))
 if __name__=='__main__':
-    #move_camera=MoveCamera()
-    #move_camera.run()
+    move_camera=MoveCamera()
+    move_camera.run()
 
     #dragon=Dragon()
     #dragon.run()
@@ -110,22 +110,3 @@ if __name__=='__main__':
 
     #chicken=Chicken()
     #chicken.run()
-    
-    #4 north
-    #0 east
-    #facing east
-    driveBase.settings(straight_speed=100, turn_rate=90)
-    DriveStraightAction(170).run()
-    DriveTurnAction(-90).run()
-    DriveStraightAction(530).run()
-    DriveTurnAction(-20).run()
-    DriveStraightAction(5).run()
-    GetToPink().run()
-    DriveTurnAction(20).run()
-    DriveStraightAction(-300).run()
-    SpinMotor(200, -180).run()
-    DriveTurnAction(-90).run()
-    DriveStraightAction(230).run()
-    DriveTurnAction(90).run()
-    DriveStraightAction(15).run()
-    Dragon().run()
