dd81d67cdec6f3c669576267d260fa6be530b155
NavierSupremecy <147449221+NavierSupremecy@users.noreply.github.com>
2023-10-10 16:46:46 -0600

    Add files via upload

--- Vivian_Mission.py
+++ Vivian_Mission.py
@@ -1,5 +1,14 @@
 from PortMap import *
 from BasicDriveActions import *
-DriveStraightAction(685).run()
+DriveStraightAction(680).run()
 DriveTurnAction(45).run()
-DriveStraightAction(100).run()
+DriveStraightAction(120).run()
+DriveStraightAction(-150).run()
+DriveTurnAction(-145).run()
+DriveStraightAction(1050).run()
+DriveTurnAction(45).run()
+DriveStraightAction(200).run()
+DriveTurnAction(-90).run()
+DriveStraightAction(240).run()
+DriveTurnAction(-45).run()
+DriveStraightAction(260).run()
