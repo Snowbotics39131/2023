153ad7b52e233fbc824a704ac5260791066aa18d
NavierSupremecy <147449221+NavierSupremecy@users.noreply.github.com>
2023-10-10 15:43:25 -0600

    Add files via upload

--- Vivian_Mission.py
+++ Vivian_Mission.py
@@ -3,4 +3,6 @@ from BasicDriveActions import *
 for i in range (4):
     DriveStraightAction(304.8).run()
     SpinMotor(500,90).run()
-    DriveTurnAction(-90).run()
+    DriveTurnAction(-100).run()
+DriveStraightAction(152.4).run()
+SpinMotor(100, 180).run()
