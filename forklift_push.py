e8ca7e71c34947d31b8c483592c261e6ee4b99f4
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-10-14 17:07:51 -0600

    Add files via upload

--- forklift_push.py
+++ forklift_push.py
@@ -5,13 +5,15 @@ from BasicDriveActions import *
 from MissionBase import *
 class PushTray(MissionBase):
     def routine(self):
+        start_db_settings=driveBase.settings()
         self.runAction(DriveStraightAction(400))
         self.runAction(DriveTurnAction(-45))
         self.runAction(DriveStraightAction(250))
         self.runAction(DriveTurnAction(-45))
-        self.runAction(DriveStraightAction(540))
+        self.runAction(DriveStraightAction(550))
         driveBase.settings(turn_rate=90)
         self.runAction(DriveTurnAction(90))
+        driveBase.settings(*start_db_settings)
 if __name__=='__main__':
     push_tray=PushTray()
     push_tray.run()
