734771b4bc2e7d3aae13c9affce1f29ba6b5b652
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-10-19 16:20:05 -0600

    Delete settingstest.py

--- settingstest.py
+++
@@ -1,7 +0,0 @@
-from PortMap import *
-startsettings=driveBase.settings()
-print(driveBase.settings())
-driveBase.settings(straight_speed=100, turn_rate=90)
-print(driveBase.settings())
-driveBase.settings(*startsettings)
-print(driveBase.settings())
