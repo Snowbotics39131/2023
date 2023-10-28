c67435a69c3d49fde4790b7e97c9e64948e952cb
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-10-27 23:57:22 -0600

    Delete unneeded files (#29)
    * Delete DriveClass.py
    
    * Delete Drivebase.py
    
    * Delete Kinematics.py
    
    * Delete LegoTest.py
    
    * Delete Mission1.py
    
    * Delete Mission2.py
    
    * Delete MultiFile.py
    
    * Delete PortMap_for_BUGBOT.py
    
    * Delete Tools.py
    
    * Delete autobalance.py
    
    * Delete autobalancePID.py
    
    * Delete bmptestcode.py
    
    * Delete door.py
    
    * Delete jacksonprogramyes.py
    
    * Delete scudscudscuddingscud.py
    
    * Delete test_6_1_2023_2.py
    
    * Delete timisnotatacovender.py
    
    * Delete tracker.py

--- MultiFile.py
+++
@@ -1,8 +0,0 @@
-from pybricks.hubs import InventorHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-hub = InventorHub()
-
