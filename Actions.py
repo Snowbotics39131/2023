67a9e3114320eb3112e22bcef969e109e967ea6f
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-11-17 01:27:13 -0700

    Add files via upload

--- Actions.py
+++ Actions.py
@@ -22,6 +22,45 @@ class Action:
         while not self.isFinished():
             self.update()
         self.done()
+def update_dec(func):
+    def output(self, *args, **kwargs):
+        time=self._stopwatch.time()
+        if time<self.period:
+            self._update_rate_flag=True
+        else:
+            if not self._update_rate_flag:
+                print(f'update rate too slow: {time}/{self.period}ms')
+            self._update_rate_flag=False
+            if not self.is_interrupted:
+                func(self, *args, **kwargs)
+    return output
+class ActionPlus:
+    def __init__(self, period=100):
+        self.period=period
+        self.is_interrupted=False
+        self._stopwatch=StopWatch()
+        self._update_rate_flag=True
+    def start(self):
+        self._stopwatch.reset()
+    @update_dec
+    def update(self):
+        pass
+    #This is the only one that really needs to be implemented as it returns something.
+    def isFinished(self):
+        raise NotImplementedError
+    def done(self):
+        pass
+    def run(self):
+        self.start()
+        while not self.isFinished():
+            self.update()
+        self.done()
+    def interrupt(self):
+        self.is_interrupted=True
+    def resume(self):
+        self.is_interrupted=False
+    def isInterrupted(self):
+        return self.is_interrupted
  
 class ParallelAction(Action): #child class of class Action
  
