e1f9e9bd9261e53ccdb35acb9d2bfc461fbc7354
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2023-12-07 15:58:05 -0700

    Update GoToBlueHomeOneWay.py

--- GoToBlueHomeOneWay.py
+++ GoToBlueHomeOneWay.py
@@ -4,13 +4,14 @@ from AdvancedActions import *
 from MissionBase import *
 class GoToBlueHome(MissionBase):
     def routine(self):
-        self.runAction(DriveCurveAction(300, 90))
-        self.runAction(DriveStraightAction(65))
-        self.runAction(DriveCurveAction(230, -90))
-        self.runAction(DriveCurveAction(-260, -90))
+        self.runAction(DriveCurveAction(270, 90))
+        self.runAction(DriveStraightAction(95))
         self.runAction(DriveStraightAction(350))
         self.runAction(DriveTurnAction(20))
-        motorBack.run_angle(200, 180)
+        try:
+            motorBack.run_angle(200, 180)
+        except:
+            hub.speaker.beep() #We have an error but we're in the middle of a competition and we don't want to waste time or lose a precision token.
         self.runAction(DriveCurveAction(100, -20))
         driveBase.straight(245)
         driveBase.turn(30)
